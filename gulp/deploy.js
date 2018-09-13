27dc3736932aec92b1f887be9bc5b6c114c52225
杨鹏博 <obpt@sina.com>
2018-09-14 00:38:02 +0800

    add deploy task

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,57 +1,57 @@
-const gulp=require('gulp');
+const gulp = require('gulp');
 const path = require('path');
 
 const PluginError = require('plugin-error');
 const log = require('fancy-log');
 const clean = require('gulp-clean')
 const zip = require('gulp-zip');
+const ftp=require('gulp-ftp');
+var sequence = require('gulp-sequence')
 const webpack = require('webpack');
-const deploy_dir_name='deploy';
+const deploy_dir_name = 'deploy';
 var package = require('../package.json')
 var webpackConfDev = require('../webpack/dev.js');
 var webpackConfProd = require('../webpack/prod.js');
 
 
-gulp.task('pack-dev',(done)=>{
-    webpack(webpackConfDev, function (err, stats) {
+gulp.task('pack-dev', (done) => {
+    return webpack(webpackConfDev, function (err, stats) {
         if (err) throw new PluginError('webpack', err);
-        log('[webpack]', stats.toString({colors: true}));
+        log('[webpack]', stats.toString({ colors: true }));
         done();
     });
 });
-gulp.task('pack-prod',(done)=>{
-    webpack(webpackConfProd, function (err, stats) {
+gulp.task('pack-prod', (done) => {
+    return webpack(webpackConfProd, function (err, stats) {
         if (err) throw new PluginError('webpack', err);
-        log('[webpack]', stats.toString({colors: true}));
+        log('[webpack]', stats.toString({ colors: true }));
         done();
     });
 });
-gulp.task('zip-dev',['pack-dev'],()=>{
-    gulp.src(path.resolve(webpackConfDev.output.path ,'*'))
-    .pipe(zip(`${package.name}_dev_${package.version}.zip`))
-    .pipe(gulp.dest(deploy_dir_name))
+gulp.task('zip-dev', ['pack-dev'], () => {
+    const timeStamp=dateFtt('yyMMdd_hhmmss',new Date())
+    return gulp.src(path.resolve(webpackConfDev.output.path, '*'))
+        .pipe(zip(`${package.name}_SNAPSHOT_${package.version}_${timeStamp}.zip`))
+        .pipe(gulp.dest(deploy_dir_name))
 });
-gulp.task('zip-prod',['pack-prod'],()=>{
-    gulp.src(path.resolve(webpackConfProd.output.path ,'*'))
-    .pipe(zip(`${package.name}_prod_${package.version}.zip`))
-    .pipe(gulp.dest(deploy_dir_name))
+gulp.task('zip-prod', ['pack-prod'], () => {
+    const timeStamp=dateFtt('yyMMdd_hhmmss',new Date())
+    return gulp.src(path.resolve(webpackConfProd.output.path, '*'))
+        .pipe(zip(`${package.name}_RC${package.version}_${timeStamp}.zip`))
+        .pipe(gulp.dest(deploy_dir_name))
 });
-gulp.task('clean', function() {
-    gulp.src([path.resolve(deploy_dir_name ,'*')], {read: false})
+gulp.task('clean', function () {
+    return gulp.src([path.resolve(deploy_dir_name, '*')], { read: false })
         .pipe(clean());
 });
-gulp.task('deploy-dev',['clean','zip-dev'],()=>{
-    log('do dev deploy ...');
-});
-gulp.task('deploy-prod',['clean', 'zip-prod'],()=>{
-    log('do prod deploy ...');
-});
+gulp.task('ftp',function(){
+   return gulp.src(path.resolve(deploy_dir_name, '*.zip'))
     .pipe(ftp({
       host: '127.0.0.1',
       port: 21,
-      user: 'yangpengbo',
-      pass:'yangsi271828@',
-      remotePath: "/"
+    //   user: 'anonymous',
+    //   pass:'@anonymous',
+      remotePath: package.name
     }));
 });
 gulp.task('deploy-dev', (cb) => {
